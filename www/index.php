5db65f6a55fd5a077ef36b35e27c6f96c0266827
khali-fa <akinodeq@gmail.com>
2019-05-04 15:53:30 +0100

    Removed Redundant files

---
+++ www/index.php
@@ -0,0 +1,41 @@
+<?php
+if(isset($_GET['message'])){
+	echo $_GET['message'];
+	}
+if (isset ($_GET['mes'])){
+	echo $_GET['mes'];
+	
+	}
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Dashboard</title>
+</head>
+
+<body>
+<h1>Welcome to our portal</h1>
+<a href="index.php">Home</a>
+<a href="about.php">About Us</a>
+<a href="contact.php">Contact Us</a>
+<a href="payment.php">Payment</a>
+<a href="result.php">Result</a>
+<hr/>
+
+
+</body>
+</html>
